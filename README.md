21ab5223a10dc6bb2dc576f964e8b870634c4e64
Gibsons Gibson <gibsonsgibson88@gmail.com>
2023-09-03 09:35:58 +0300

    scss

--- README.md
+++
@@ -1,2 +0,0 @@
-# eccomerce-masterpiece
-A complete mastery of MERN stack technology- An eccomerce example
---
+++ frontend/src/components/header/Header.js
@@ -0,0 +1,10 @@
+//eslint-disable-next-line
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
