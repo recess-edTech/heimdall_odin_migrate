571a09cde5f172a2ef1df3f44f155617b46869f7
Domains18 <gibsonsgibson88@gmail.com>
2024-03-27 11:51:48 +0300

    Delete unnecessary files and configurations

--- README.md
+++
@@ -1,4 +0,0 @@
-# say hello function
-
-- just a simple function that returns a string "Hello, World!"
-
--- src/functions.ts
+++
@@ -1,9 +0,0 @@
-import { SayHelloProps } from "./types"
-
-export function sayHello({firstName, lastName, age}:SayHelloProps) {
-    console.log(`values, ${firstName}`)
-    console.log(age)
-    if (lastName) {
-        console.log(`values ${lastName}`)
-    }
-}
